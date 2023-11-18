8c9b8d3b3bc7766a11202131013821929d7a292e
Shivam <shivam@Shivams-MacBook-Pro.local>
2023-11-18 16:05:23 +0530

    del canvas

--- app/components/canvas/index.tsx
+++
@@ -1,2 +0,0 @@
-export { default as HeartCanvas } from "./HeartCanvas";
-export { default as DiwaliCanvas } from "./DiwaliCanvas";
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import { DiwaliCanvas, HeartCanvas } from "./components/canvas";
+// import { DiwaliCanvas, HeartCanvas } from "./components/canvas";
 
 export default function Home() {
   return (
     <main>
-      <HeartCanvas />
-      {/* <DiwaliCanvas /> */}
       <h1>Hello World</h1>
     </main>
   );
